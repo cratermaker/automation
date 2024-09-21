42df01a58796d7e3936c57da89fcf129878eb3c2
quinnielmendoza <119793833+quinnielmendoza@users.noreply.github.com>
2024-09-21 23:07:21 +0800

    Create mysqlxampp.bat

---
+++ mysqlxampp.bat
@@ -0,0 +1,16 @@
+@echo off
+:: Set the backup date format
+set backupdate=%date:~10,4%%date:~7,2%%date:~4,2%
+
+:: Perform Database Backup
+echo Performing Database Backup...
+:: Wait for a moment while backup is in progress...
+echo Wait for a moment while the backup is in progress...
+
+:: Run the mysqldump commands
+"X:\www\mysql\bin\mysqldump" -u root ip > C:\daily_Backups\dbname%backupdate%.sql
+"X:\www\mysql\bin\mysqldump" -u root ip > D:\dbname%backupdate%.sql
+echo Done backing up IP Threat database!
+
+:: Exit the script
+exit /b
